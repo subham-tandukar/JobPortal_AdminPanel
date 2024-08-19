b2360a64be8a8aaaf18f93b7c738170134b1cff1
Subham <subhamtndkr9@gmail.com>
2024-08-19 23:42:45 +0545

    joblist update

--- src/pages/Job/JobList.jsx
+++ src/pages/Job/JobList.jsx
@@ -197,7 +197,7 @@ const JobList = () => {
     },
     {
       name: "No. of applications",
-      width: "130px",
+      width: "100px",
       center: true,
       selector: (row) => (
         <>
@@ -218,7 +218,7 @@ const JobList = () => {
     },
     {
       name: "Featured",
-      width: "130px",
+      width: "100px",
       center: true,
       selector: (row) => (
         <Tippy
@@ -239,7 +239,7 @@ const JobList = () => {
     },
     {
       name: "Status",
-      width: "130px",
+      width: "100px",
       center: true,
       selector: (row) => (
         <Tippy
