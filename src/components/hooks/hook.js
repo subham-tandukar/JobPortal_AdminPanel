2d789949f1dace67fc3fc820056d2941317be1a9
Subham <subhamtndkr9@gmail.com>
2024-08-19 23:09:12 +0545

    base url updated

--- src/components/hooks/hook.js
+++ src/components/hooks/hook.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const baseURL = process.env.REACT_APP_URL;
-export const baseURL = "http://localhost:8009";
+export const baseURL = process.env.REACT_APP_URL;
+// export const baseURL = "http://localhost:8009";
 
 // Reusable function to handle API requests
 export const apiRequest = async (url, data, thunkAPI) => {
